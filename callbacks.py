17688294bc1e8e95d5067575b3380ff2aeff7b1a
Mikael de Verdier <mikael.deverdier@gmail.com>
2024-11-24 17:43:03 +0100

    fix to validation metrics; minor imrpvoements

--- callbacks.py
+++ callbacks.py
@@ -21,9 +21,10 @@ class ModelCheckpointWithHistory(ModelCheckpoint):
             if not os.path.exists(folder_to_check):
                 os.mkdir(folder_to_check)  # why doesn't os.mkdir just create all folders?
 
-    def append_to_history(self, old_logs, logs):
+    def append_to_history(self, old_logs, logs, is_test=False):
         appended_logs = {
-            key: old_logs.get(key, []) + (value if isinstance(value, list) else [value])  # ugly
+            f"val_{key}" if is_test else key:
+            old_logs.get(f"val_{key}" if is_test else key, []) + (value if isinstance(value, list) else [value])  # ugly
             for key, value in logs.items()
         }
         new_logs = old_logs | appended_logs
@@ -31,17 +32,17 @@ class ModelCheckpointWithHistory(ModelCheckpoint):
         return new_logs
 
     def on_train_batch_end(self, batch, logs=None):
+        self.history = self.append_to_history(self.history, logs)
+
         copy_self = copy(self)  # need to copy because calling _should_save_on_batch has a persistent impact
         if copy_self._should_save_on_batch(batch):
-            self.history = self.append_to_history(self.history, logs)
-
             if self.load and os.path.exists(self.history_filepath):
                 with open(self.history_filepath, "r") as f:
                     old_metrics = json.load(f)
             else:
                 old_metrics = {}
 
-            used_metrics = self.append_to_history(old_metrics, self.history)
+            used_metrics = self.append_to_history(old_metrics, self.history)  # validation metrics are 1 behind when saving...
             with open(self.history_filepath, "w") as f:
                 json.dump(used_metrics, f)
 
@@ -51,6 +52,6 @@ class ModelCheckpointWithHistory(ModelCheckpoint):
         super().on_train_batch_end(batch, logs)
 
     def on_test_batch_end(self, batch, logs=None):
-        self.history = self.append_to_history(self.history, logs)
+        self.history = self.append_to_history(self.history, logs, is_test=True)
 
         super().on_test_batch_end(batch, logs)

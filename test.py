3503bf04986f25c798adf69526a1a24d76b665f1
Mikael de Verdier <mikael.deverdier@gmail.com>
2025-01-29 02:12:13 +0100

    minor improvements

--- test.py
+++ test.py
@@ -3,12 +3,21 @@ from model.street_clip.street_clip_model import StreetCLIP
 from test_components.evalutator import Evaluator
 
 if __name__ == "__main__":
+    # # Test a GeoCLIP model
     # geo_clip = GeoCLIP()
     # evaluator = Evaluator()
 
     # evaluator.evaluate(geo_clip)
 
+    # # Test a StreetCLIP model with new prompt format
+    # steet_clip = StreetCLIP()
+    # evaluator = Evaluator(processor_method="Street")
+
+    # evaluator.evaluate(steet_clip)
+
+    # Test a StreetCLIP model with original prompt format
     steet_clip = StreetCLIP()
     evaluator = Evaluator(processor_method="StreetOG")
 
     evaluator.evaluate(steet_clip)
+
--- train.py
+++ train.py
@@ -3,12 +3,21 @@ from model.street_clip.street_clip_model import StreetCLIP
 from train_components.trainer import Trainer
 
 if __name__ == "__main__":
+    # Train a GeoCLIP model from scratch
     geo_clip = GeoCLIP()
     trainer = Trainer()
 
     trainer.train(geo_clip)
 
+    # # Fine-tune a StreetCLIP model with new prompt format
     # street_clip = StreetCLIP()
     # trainer = Trainer(processor_method="Street")
 
     # trainer.train(street_clip)
+
+    # # Fine-tune a StreetCLIP model with original prompt format
+    # street_clip = StreetCLIP()
+    # trainer = Trainer(processor_method="StreetOG")
+
+    # trainer.train(street_clip)
+

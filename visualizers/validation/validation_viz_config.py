586e75aba740945980cb96869512efd86530a173
Mikael de Verdier <mikael.deverdier@gmail.com>
2025-02-09 13:39:18 +0100

    improved validation_visualizer with option to plot stacked bar chart; minor improvements

--- visualizers/validation/validation_viz_config.py
+++ visualizers/validation/validation_viz_config.py
@@ -1,9 +1,8 @@
 # Visualizer configuration
 save_path = "save_folder/validation_visualization.png"
 validation_file = "save_folder/validation_results.json"
-example_prompts = ["Hello World"]
-example_input_shape = (1, 3, 336, 336)
 
 # Visualization arguments
-grouping_size = 5
+grouping_size = 2
 plot_density = True
+plot_stacked = True
